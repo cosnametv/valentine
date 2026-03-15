b9911d6c72e18ce7a22d270e28464b6814a8247e
cosnametv <josephwaweru4967@gmail.com>
2026-03-15 15:33:32 +0300

    youtube video Preview

--- components/SeeMore.tsx
+++ components/SeeMore.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export const SeeMore: React.FC = () => {
   return (
-    <div className="flex justify-center py-8">
+    <div className="flex justify-center">
       <Link
         to="/see-more"
-        className="px-8 py-4 rounded-2xl bg-rose-500 hover:bg-rose-600 text-white font-semibold tracking-wide shadow-lg hover:shadow-xl transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2 inline-block"
+        className="px-3 py-1 rounded-2xl bg-rose-500 hover:bg-rose-600 text-white font-semibold tracking-wide shadow-lg hover:shadow-xl transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-rose-400 focus:ring-offset-2 inline-block"
       >
         See More
       </Link>
